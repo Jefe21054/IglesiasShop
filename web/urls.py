31a1377bd756ef2bc0cdda4057c93e4993f77c5f
Jefe21054 <ivaniglesias500@protonmail.ch>
2023-03-26 18:25:06 -0500

    Creación de vista para el detalle de producto

--- web/urls.py
+++ web/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('productosPorCategoria/<int:categoria_id>',views.productosPorCategoria,name='productosPorCategoria'),
     path('productosPorNombre',views.productosPorNombre,name='productosPorNombre'),
-]
+    path('producto/<int:producto_id>',views.productoDetalle,name='producto'),
+    path('categ/<int:categoria_id>', views.categ,name='categ'),
+] 
--- web/views.py
+++ web/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Categoria,Producto
 
 # Create your views here.
-''' VISTA PARA EL CATALOGO DE PRODUCTOS '''
 
 def index(request):
+    ''' VISTA PARA EL CATALOGO DE PRODUCTOS '''
+
     listaCategorias = Categoria.objects.all()
     listaProductos = Producto.objects.all()
 
@@ -17,7 +18,8 @@ def index(request):
 
 def productosPorCategoria(request,categoria_id):
     ''' VISTA PARA FILTRAR PRODUCTOS POR CATEGORIA'''
-    objCategoria = Categoria.objects.get(pk=categoria_id)
+    #objCategoria = Categoria.objects.get(pk=categoria_id)
+    objCategoria = get_object_or_404(Categoria,pk=categoria_id)
     listaProductos = objCategoria.producto_set.all()
     listaCategorias = Categoria.objects.all()
 
@@ -30,7 +32,8 @@ def productosPorCategoria(request,categoria_id):
 
 def productosPorNombre(request):
     ''' VISTA PARA FILTRAR PRODUCTOS POR NOMBRE '''
-    nombre = request.POST['nombre']
+    llega = request.POST['nombre']
+    nombre = llega.upper()
     
     listaProductos = Producto.objects.filter(nombre__contains=nombre)
     listaCategorias = Categoria.objects.all()
@@ -41,3 +44,24 @@ def productosPorNombre(request):
     }
 
     return render(request,'index.html',context)
+
+def productoDetalle(request,producto_id):
+    ''' VISTA PARA EL DETALLE DE PRODUCTO '''
+
+    objProducto = get_object_or_404(Producto,pk=producto_id)
+    categ = objProducto.categoria
+    objCategoria = get_object_or_404(Categoria,nombre=categ)
+
+    context = {
+        'producto':objProducto,
+        'categ':objCategoria,
+    }
+
+    return render(request,'producto.html',context)
+
+def categ(request,categoria_id):
+    ''' VISTA PARA REDIRECCION CATEGORIAS '''
+
+    objCategoria = get_object_or_404(Categoria,pk=categoria_id)
+    
+    return redirect('/productosPorCategoria/'+str(categoria_id))
