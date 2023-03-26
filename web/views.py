0431049396f64577757c8087d15c480e437b63d0
Jefe21054 <ivaniglesias500@protonmail.ch>
2023-03-26 11:11:12 -0500

    Creación de vista para filtrado de productos por categoría

--- web/views.py
+++ web/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Categoria,Producto
 
 # Create your views here.
+''' VISTA PARA EL CATALOGO DE PRODUCTOS '''
+
+def index(request):
+    listaCategorias = Categoria.objects.all()
+    listaProductos = Producto.objects.all()
+
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos,
+    }
+
+    return render(request,'index.html',context)
+
+def productosPorCategoria(request,categoria_id):
+    ''' VISTA PARA FILTRAR PRODUCTOS POR CATEGORIA'''
+    objCategoria = Categoria.objects.get(pk=categoria_id)
+    listaProductos = objCategoria.producto_set.all()
+    listaCategorias = Categoria.objects.all()
+
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos,
+    }
+
+    return render(request,'index.html',context)
