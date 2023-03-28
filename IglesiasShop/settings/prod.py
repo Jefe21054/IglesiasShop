331fb2ad04064c52c7730296965cdf4e13dccd51
Jefe21054 <ivaniglesias500@protonmail.ch>
2023-03-28 12:10:49 -0500

    Configuración de entorno local y de producción

--- IglesiasShop/settings/prod.py
+++ IglesiasShop/settings/prod.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ag6p*o@dx3jp2^v240l2!#2&vcm9%ivch21ai7&og%s=)-*2-h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
